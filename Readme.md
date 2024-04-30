d3bc792fa0cd2f5e8c76e8e50eff1c4c0401afaa
akshatmittal2002 <mittalakshat.kt@gmail.com>
2024-04-30 19:32:23 +0530

    Initial Commit

---
+++ Readme.md
@@ -0,0 +1,27 @@
+# Neural Networks and Its Applications
+- This is a tutorial on Neural Networks and its applications.
+- We discuss in details about what is a Neural Network, its architecture and how it works.
+- Then we discuss an application of classifying a planar data using a Neural Network, with non-linear decision boundary and compare it with Logistic Regression.
+- We also discuss about the hyperparameters and how they affect the performance of our network.
+- Then we extend our discussion to deep neural networks and discuss an application of classifying a cat vs non-cat image using a deep neural network.
+- Finally, we conclude our discussion with some examples of complex neural networks like CNNs, RNNs, etc.
+
+### Contents
+- Please find the detailed tutorial in the [Jupyter Notebook](./Tutorial.ipynb)
+- Dataset used for trianing is downloaded from Kaggle and can be found at [Link](./Datasets)
+- Some utilities function used in the tutorial can be found at [Link](./Utils)
+
+### Python Libraries Used
+- Numpy
+- Matplotlib
+- H5py
+- Sklearn
+- Make sure to install the above libraries before running the code using following command:
+```bash
+pip install numpy matplotlib h5py scikit-learn
+```
+
+### References
+- [Dr. Andrew Ng's]((http://www.andrewng.org/)) Deep Learning Specialization course offered by [`deeplearning.ai`](https://www.deeplearning.ai/) - [Link](https://www.coursera.org/specializations/deep-learning)
+- [Dr. Arijit Sur's](https://www.iitg.ac.in/arijit/) CS_361 Machine Learning course - [Link](http://jatinga.iitg.ernet.in/~arijit/ML2023.htm).
+- Dataset Downloaded from [Kaggle](https://www.kaggle.com/datasets/muhammeddalkran/catvnoncat)
---
+++ Utils/dnn_utils.py
@@ -0,0 +1,82 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+
+
+def sigmoid(Z):    
+    A = 1/(1+np.exp(-Z))
+    cache = Z
+    return A, cache
+
+def relu(Z):
+    A = np.maximum(0,Z)
+    assert(A.shape == Z.shape)
+    cache = Z 
+    return A, cache
+
+
+def relu_backward(dA, cache):
+    Z = cache
+    dZ = np.array(dA, copy=True) # just converting dz to a correct object.
+    # When z <= 0, you should set dz to 0 as well. 
+    dZ[Z <= 0] = 0
+    assert (dZ.shape == Z.shape)
+    return dZ
+
+def sigmoid_backward(dA, cache):
+    Z = cache
+    s = 1/(1+np.exp(-Z))
+    dZ = dA * s * (1-s)
+    assert (dZ.shape == Z.shape)    
+    return dZ
+
+def load_data():
+    train_dataset = h5py.File('Datasets/train_catvnoncat.h5', "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+
+    test_dataset = h5py.File('Datasets/test_catvnoncat.h5', "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+
+    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+    
+    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+    
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+
+def linear_forward(A, W, b):    
+    Z = W.dot(A) + b
+    
+    assert(Z.shape == (W.shape[0], A.shape[1]))
+    cache = (A, W, b)
+    
+    return Z, cache
+
+def linear_backward(dZ, cache):
+    A_prev, W, b = cache
+    m = A_prev.shape[1]
+
+    dW = 1./m * np.dot(dZ,A_prev.T)
+    db = 1./m * np.sum(dZ, axis = 1, keepdims = True)
+    dA_prev = np.dot(W.T,dZ)
+    
+    assert (dA_prev.shape == A_prev.shape)
+    assert (dW.shape == W.shape)
+    assert (db.shape == b.shape)
+    
+    return dA_prev, dW, db
+
+def print_mislabeled_images(classes, X, y, p):
+    a = p + y
+    mislabeled_indices = np.asarray(np.where(a == 1))
+    plt.rcParams['figure.figsize'] = (40.0, 40.0) # set default size of plots
+    num_images = len(mislabeled_indices[0])
+    for i in range(num_images):
+        index = mislabeled_indices[1][i]
+        
+        plt.subplot(2, num_images, i + 1)
+        plt.imshow(X[:,index].reshape(64,64,3), interpolation='nearest')
+        plt.axis('off')
+        plt.title("Prediction: " + classes[int(p[0,index])].decode("utf-8") + " \n Class: " + classes[y[0,index]].decode("utf-8"))
